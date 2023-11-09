ad0f16d54a05fb8924fdeca3282fb289107c7c74
Naimur Rahman <naimur2231@gmail.com>
2023-11-09 10:33:52 +0600

    learn Encapsulation opp in typesript

---
+++ module-3/src/Encapsulation.ts
@@ -0,0 +1,36 @@
+{
+  //Encapsulation
+  class BankAccount {
+    readonly id: number;
+    name: string;
+    protected balance: number;
+
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this.balance = balance;
+    }
+
+    addDeposit(amount: number) {
+      this.balance = this.balance + amount;
+    }
+
+    getBalance() {
+      return this.balance;
+    }
+  }
+
+  class StudentAccount extends BankAccount {
+    test() {
+      this.balance;
+    }
+  }
+
+  const user1 = new BankAccount(123, "Mr. x", 4000);
+  //   user1.id = 343;
+  //   user1.balance = 3253;
+
+  user1.addDeposit(500);
+  const res1 = user1.getBalance();
+  console.log(res1);
+}
